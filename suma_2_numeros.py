e07b3008f3f54792bd9514ba31efbd80d703e56c
Sabarius <134521965+Sabarius@users.noreply.github.com>
2024-04-23 14:42:20 -0600

    Create suma_2_numeros.py

---
+++ suma_2_numeros.py
@@ -0,0 +1,5 @@
+numero1 = input("Ingresa el primer numero: ")
+numero2 = input("Ingresa el segundo numero: ")
+
+suma = int(numero1) + int(numero2)
+print(f"La suma de {numero1} más {numero2} es: {suma}")
