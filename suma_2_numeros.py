825ea3b4b3eecb4f24f64b4ba40a8240aed9a71a
Sabarius <134521965+Sabarius@users.noreply.github.com>
2024-04-23 15:13:10 -0600

    Delete suma_2_numeros.py

--- suma_2_numeros.py
+++
@@ -1,5 +0,0 @@
-numero1 = input("Ingresa el primer numero: ")
-numero2 = input("Ingresa el segundo numero: ")
-
-suma = int(numero1) + int(numero2)
-print(f"La suma de {numero1} más {numero2} es: {suma}")
